bf4d1118495b2402ef0b3d629a40a9b6b03481c0
Mona <2829922e@student.gla.ac.uk>
2024-08-17 15:09:44 -0700

    Partial Solution

--- 88_Merge_Sorted_Array.js
+++ 88_Merge_Sorted_Array.js
@@ -10,9 +10,21 @@
 
 var merge = function(nums1, m, nums2, n) {
 
+    //Adds the items from nums2 into nums1 without sorting
+    //No zeroes added from nums2 but nums1 zeroes are still there
     for (let i = 0; i < nums2.length; i++){
         if (nums2[i] > 0){
             nums1.push(nums2[i])
+        }
+    }
+
+    //Sorts the numbers
+    nums1.sort();
+
+    //Removes some of the zeroes but not all
+    for (let i = 0; i <= nums1.length; i++){
+        if (nums1[i] === 0) {
+            nums1.splice(i, 1); 
         }
     }
 }
