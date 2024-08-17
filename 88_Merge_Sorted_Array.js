30392cda4ebb470b5fd696c423cfe6cffb87ec66
Mona <2829922e@student.gla.ac.uk>
2024-08-17 15:07:07 -0700

    Merged Unsorted

---
+++ 88_Merge_Sorted_Array.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+
+
+var merge = function(nums1, m, nums2, n) {
+
+    for (let i = 0; i < nums2.length; i++){
+        if (nums2[i] > 0){
+            nums1.push(nums2[i])
+        }
+    }
+}
