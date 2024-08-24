50905dde1cb7980f4acf2862a997314d29a4d068
Mona <2829922e@student.gla.ac.uk>
2024-08-24 03:02:02 -0700

    first attempt

--- 27_Remove_Element.js
+++ 27_Remove_Element.js
@@ -5,8 +5,19 @@
  */
 
 
+let nums = [3,2,2,3];
+let val = 3;
+
 var removeElement = function(nums, val) {
 
-    
+    for(let i = 0; i <= nums.length, i++;){
+        if (nums[i] === val) {
+            nums[i] = 101;
+        }
+    }
+
+    return nums;
     
 };
+
+console.log(removeElement(nums, val)); 
