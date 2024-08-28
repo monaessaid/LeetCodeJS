a506fd3dd1b111009971c673bb3298d31aeb1028
Mona <2829922e@student.gla.ac.uk>
2024-08-27 19:08:27 -0700

    potential solution

--- 27_Remove_Element.js
+++ 27_Remove_Element.js
@@ -9,15 +9,15 @@ let nums = [3,2,2,3];
 let val = 3;
 
 var removeElement = function(nums, val) {
-
-    for(let i = 0; i <= nums.length; i++){
-        if (nums[i] === val) {
-            
-        }
-    }
-
-    return nums;
     
+    let x = 0;
+    for(let i = 0; i <= nums.length; i++){
+            if (nums[i] != val) {
+                nums[x] = nums[i];
+                x++;
+            }
+        }
+        return x;    
 };
 
 console.log(removeElement(nums, val)); 
